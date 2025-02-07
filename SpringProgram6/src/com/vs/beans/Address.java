cd709b1690752f1f8c107cdc7bf7b8808e152b98
vaibhavshende03 <jitcsevaibhavshende@gmail.com>
2025-02-07 15:24:43 +0530

    Constructor DI

---
+++ SpringProgram6/src/com/vs/beans/Address.java
@@ -0,0 +1,28 @@
+package com.vs.beans;
+
+public class Address {
+	private int houseno;
+	private String city;
+	private int pincode;
+	
+	
+	
+	
+	public Address(int houseno, String city, int pincode) {
+		super();
+		this.houseno = houseno;
+		this.city = city;
+		this.pincode = pincode;
+	}
+
+
+
+
+	@Override
+	public String toString() {
+		return "houseno=" + houseno + ", city=" + city + ", pincode=" + pincode ;
+	}
+	
+	
+	
+}
