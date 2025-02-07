ff9c76a05e8cf45c81ecfd303d07a0f671705197
vaibhavshende03 <jitcsevaibhavshende@gmail.com>
2025-02-07 15:02:53 +0530

    DI using Setter Method

---
+++ SpringProgram5/src/com/vs/main/Main.java
@@ -0,0 +1,19 @@
+package com.vs.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.vs.beans.Student;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String configLocation="/com/vs/resources/applicationContext.xml";
+		ApplicationContext context=new ClassPathXmlApplicationContext(configLocation);
+		
+		Student std=(Student) context.getBean("stdId");
+		std.display();
+	}
+
+}
