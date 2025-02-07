3bca8ee8002a930c7a9f83f7831236fd208afa5f
vaibhavshende03 <jitcsevaibhavshende@gmail.com>
2025-02-07 15:00:13 +0530

    Annotation based with file configuration

--- SpringProgram4/src/com/vs/resources/SpringConfigFile.java
+++ SpringProgram4/src/com/vs/resources/SpringConfigFile.java
@@ -1,9 +1,10 @@
 package com.vs.resources;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
-@Component
+@Configuration
 @ComponentScan("com.vs.beans")
 //@ComponentScan({"com.vs.beans","----","----"})  // if we have multiple location 	 
 //@ComponentScan(basePackages = {"com.vs.beans"})
